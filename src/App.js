a3f90cbac6d406643d5918ed560cc7f77ab45cfb
subhamengine <subhamengine@gmail.com>
2024-09-23 13:43:19 +0530

    hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 function App() {
   return (
     <div className="">
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero/>
       </div>
       <ButtonGradient />
     </div>
